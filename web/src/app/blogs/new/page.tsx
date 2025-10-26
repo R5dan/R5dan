ca8d49dac8bd622844b5ab4a92dd0fa396d81d78
R5dan <r5dan@outlook.com>
2025-10-26 01:49:40 +0100

    trpc

---
+++ web/src/app/blogs/new/page.tsx
@@ -0,0 +1,15 @@
+// import { api } from "../../../../convex/_generated/api";
+// import { useMutation, useQuery } from "convex/react";
+
+// export default function Page() {
+//   const user = useQuery(api.auth.getCurrentUser);
+//   const createBlog = useMutation(api.blogs.addBlog);
+  
+//   if (!user) {
+//     return <>Unauthed</>
+//   }
+//   if (user.)
+
+//   return (
+//     <div></div>
+// }
