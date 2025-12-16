b7fe27bb1c71863f9debab1b1b9bf451a08ce0b0
R5dan <r5dan@outlook.com>
2025-12-16 18:16:00 +0000

    gh cmd

---
+++ apps/cli/src/gh/index.ts
@@ -0,0 +1,14 @@
+import { Command } from "commander";
+import open from "open";
+
+export const ghCommand = new Command("gh");
+
+ghCommand.description("GitHub commands");
+
+ghCommand.command("open").alias("o").action(async () => {
+  open("https://github.com")
+})
+
+ghCommand.action(async () => {
+  Bun.spawn(["lazygit"]);
+})
