24b325e38b19df70cf88ef4d395c85929f5a623b
R5dan <r5dan@outlook.com>
2025-12-16 18:16:47 +0000

    register gh cmd

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,9 +1,11 @@
 import { Command } from "commander";
 import { chatCommand } from "./commands/chat";
 import { initCommand } from "./commands/init";
+import { ghCommand } from "./gh";
 
 
 export const cli = new Command("r5dan");
 cli.addCommand(chatCommand);
 cli.addCommand(initCommand);
+cli.addCommand(ghCommand);
 cli.parse();
