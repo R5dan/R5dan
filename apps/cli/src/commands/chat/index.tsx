cf29c9b2424a791eb2d0342d7755cd71ae8b3a3b
R5dan <r5dan@outlook.com>
2025-12-17 01:53:47 +0000

    add cli globally

--- apps/cli/src/commands/chat/index.tsx
+++ apps/cli/src/commands/chat/index.tsx
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { Command } from "commander";
 import { render } from "ink";
-import { App } from "./test";
+import { App } from "./component";
 
 export const chatCommand = new Command("chat").action(async () => { 
   const { waitUntilExit } = render(<App />);
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,8 +1,9 @@
+#!/usr/bin / env bun
+
 import { Command } from "commander";
 import { chatCommand } from "./commands/chat";
 import { initCommand } from "./commands/init";
 import { ghCommand } from "./gh";
-
 
 export const cli = new Command("r5dan");
 cli.addCommand(chatCommand);
